a5923e331e0d799dcde543a6e42514d50fbd1d7c
Atiksh Soni <ateekshsoni@gmail.com>
2025-08-10 13:58:32 +0530

    refactor: remove SignupPage and related files
    - Deleted SignupPage component and its associated styles and logic.
    - Removed references to SignupPage in index.js.
    - Cleaned up Tailwind CSS configuration and Vite configuration files.

--- frontend-test/src/pages/DashboardOverview.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import DashboardContent from "@/components/DashboardContent";
-
-const DashboardOverview = () => {
-  return <DashboardContent />;
-};
-
-export default DashboardOverview;
--- frontend-test/src/pages/index.js
+++
@@ -1,6 +0,0 @@
-// Auth Pages
-export { default as LoginPage } from "./auth/LoginPage";
-export { default as SignupPage } from "./auth/SignupPage";
-
-// Dashboard Pages
-export { default as DashboardOverview } from "./DashboardOverview";

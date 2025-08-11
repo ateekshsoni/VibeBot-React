ab7a3bfbabcca7c7f8fc7a3801d02bcbff5bce15
Atiksh Soni <ateekshsoni@gmail.com>
2025-08-11 19:49:37 +0530

    feat: Add useEffect import to AutomationPage for future enhancements

--- testing-stuff/src/pages/AutomationPage.jsx
+++ testing-stuff/src/pages/AutomationPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useAuth } from "@clerk/clerk-react";
 import InstagramAccountCard from "@/components/InstagramAccountCard";
 import AutomationSettingsCard from "@/components/AutomationSettingsCard";
